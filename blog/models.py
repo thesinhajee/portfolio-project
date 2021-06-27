68d9f48b7b0de42305da7fbb82554b66903eccc5
Aditya Sinha <adityasinha2001@gmail.com>
2021-06-27 14:09:04 +0530

    Projects framework done with help of bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=50)
+    date=models.DateTimeField(null=True)
+    image=models.ImageField(upload_to='images/')
+    body=models.CharField(max_length=150)
